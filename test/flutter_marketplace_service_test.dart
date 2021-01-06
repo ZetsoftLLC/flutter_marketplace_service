20038b12b5d787136494b74253809134021b4b9c
turopoff <turopoff>
2021-01-06 14:09:30 +0500

    added users file --test

--- test/flutter_marketplace_service_test.dart
+++ test/flutter_marketplace_service_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_marketplace_service/models/users.model.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_marketplace_service/models/users.model.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_marketplace_service/flutter_marketplace_service.dart';
+// import 'package:flutter_marketplace_service/flutter_marketplace_service.dart';
 
-void main() {
-  test('Get users', () async {
-    List<UserModel> users = await Api.getUsers();
-  });
-}
+// void main() {
+//   test('Get users', () async {
+//     List<UserModel> users = await Api.getUsers();
+//   });
+// }
