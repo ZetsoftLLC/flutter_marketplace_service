cf80121721e5350cc1a6db4908ad5a51dc23104f
turopoff <turopoff>
2021-01-06 14:21:31 +0500

    added users api --test

--- lib/flutter_marketplace_service.dart
+++ lib/flutter_marketplace_service.dart
@@ -5,17 +5,20 @@ import 'dart:convert';
 import 'package:flutter_marketplace_service/models/users.model.dart';
 import 'package:http/http.dart' as http;
 
+part "users.dart";
+
 const baseUrl = "http://market.zetsoft.uz/rest/shop-catalog";
 
 class Api {
-  static Future<List<UserModel>> getUsers() async {
-    try {
-      String url = baseUrl + "/users";
-      var response = await http.get(url);
-      Iterable list = json.decode(response.body);
-      return list.map((model) => UserModel.fromJson(model)).toList();
-    } catch (e) {
-      return List<UserModel>();
-    }
-  }
+  // static Future<List<UserModel>> getUsers;
+  // () async {
+  //   try {
+  //     String url = baseUrl + "/users";
+  //     var response = await http.get(url);
+  //     Iterable list = json.decode(response.body);
+  //     return list.map((model) => UserModel.fromJson(model)).toList();
+  //   } catch (e) {
+  //     return List<UserModel>();
+  //   }
+  // }
 }
--- lib/users.dart
+++ lib/users.dart
@@ -1,14 +1,14 @@
-// part of flutter_marketplace_service;
+part of flutter_marketplace_service;
 
-// class _Users {
-//   static Future<List<UserModel>> getUsers() async {
-//     try {
-//       String url = baseUrl + "/users";
-//       var response = await http.get(url);
-//       Iterable list = json.decode(response.body);
-//       return list.map((model) => UserModel.fromJson(model)).toList();
-//     } catch (e) {
-//       return List<UserModel>();
-//     }
-//   }
-// }
+class Users {
+  static Future<List<UserModel>> getUsers() async {
+    try {
+      String url = baseUrl + "/users";
+      var response = await http.get(url);
+      Iterable list = json.decode(response.body);
+      return list.map((model) => UserModel.fromJson(model)).toList();
+    } catch (e) {
+      return List<UserModel>();
+    }
+  }
+}
