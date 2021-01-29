230b50d0e1cbd7641d300635a6a8a9cfaf934eba
tdav <tdav@mail.ru>
2021-01-29 18:12:15 +0500

    11

--- test/banners_service_test.dart
+++ test/banners_service_test.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter_marketplace_service/service/banner/banner_api_provider.dart';
 import 'package:flutter_marketplace_service/service/banner/banner_repository.dart';
 import 'package:flutter_marketplace_service/service/banner/cubit/banner_cubit.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,19 +6,13 @@ import 'package:flutter_test/flutter_test.dart';
 class MockBannerCubit extends MockBloc<BannerState> implements BannerCubit {}
 
 void main() {
-  test('Get brands', () async {
-    BannerApiProvider repo = BannerApiProvider();
-    var banners = await repo.getBanners();
-    assert(banners != null);
-  });
-
   group('BannersCubit', () {
     final BannersRepository repository = new BannersRepository();
     blocTest<BannerCubit, BannerState>(
-      'get list',
+      'fetchBanners',
       build: () => BannerCubit(repository),
       act: (cubit) async => cubit.fetchBanners(),
-      expect: null,
+      expect: [null],
     );
   });
 }
