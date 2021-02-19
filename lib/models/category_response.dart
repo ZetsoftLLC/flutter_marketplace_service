23bc3e41429855740a983e99c6b172c6655b4e43
turopoff <turopoff.n@gmail.com>
2021-02-19 14:27:24 +0500

    catalog page

--- lib/models/category_response.dart
+++ lib/models/category_response.dart
@@ -1,11 +1,5 @@
 import 'dart:convert';
 
-CategoryResponseModel allCategoryFromJson(String str) =>
-    CategoryResponseModel.fromJson(json.decode(str));
-
-String allCategoryToJson(CategoryResponseModel data) =>
-    json.encode(data.toJson());
-
 class CategoryResponseModel {
   CategoryResponseModel({
     this.data,
@@ -13,24 +7,36 @@ class CategoryResponseModel {
     this.status,
   });
 
-  List<CategoryModel> data;
-  bool success;
-  int status;
+  final List<CategoryModel> data;
+  final bool success;
+  final int status;
+
+  CategoryResponseModel copyWith({
+    List<CategoryModel> data,
+    bool success,
+    int status,
+  }) =>
+      CategoryResponseModel(
+        data: data ?? this.data,
+        success: success ?? this.success,
+        status: status ?? this.status,
+      );
 
   factory CategoryResponseModel.fromJson(String str) =>
       CategoryResponseModel.fromMap(json.decode(str));
 
+  String toJson() => json.encode(toMap());
+
   factory CategoryResponseModel.fromMap(Map<String, dynamic> json) =>
       CategoryResponseModel(
         data: List<CategoryModel>.from(
-          json["data"].map((x) => CategoryModel.fromJson(x)),
-        ),
+            json["data"].map((x) => CategoryModel.fromMap(x))),
         success: json["success"],
         status: json["status"],
       );
 
-  Map<String, dynamic> toJson() => {
-        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+  Map<String, dynamic> toMap() => {
+        "data": List<dynamic>.from(data.map((x) => x.toMap())),
         "success": success,
         "status": status,
       };
@@ -38,47 +44,84 @@ class CategoryResponseModel {
 
 class CategoryModel {
   CategoryModel({
+    this.id,
     this.name,
     this.banner,
     this.icon,
     this.links,
   });
 
-  String name;
-  String banner;
-  String icon;
-  LinksModel links;
+  final int id;
+  final String name;
+  final String banner;
+  final String icon;
+  final Links links;
 
-  factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+  CategoryModel copyWith({
+    int id,
+    String name,
+    String banner,
+    String icon,
+    Links links,
+  }) =>
+      CategoryModel(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        banner: banner ?? this.banner,
+        icon: icon ?? this.icon,
+        links: links ?? this.links,
+      );
+
+  factory CategoryModel.fromJson(String str) =>
+      CategoryModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory CategoryModel.fromMap(Map<String, dynamic> json) => CategoryModel(
+        id: json["id"],
         name: json["name"],
         banner: json["banner"],
         icon: json["icon"],
-        links: LinksModel.fromJson(json["links"]),
+        links: Links.fromMap(json["links"]),
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toMap() => {
+        "id": id,
         "name": name,
         "banner": banner,
         "icon": icon,
-        "links": links.toJson(),
+        "links": links.toMap(),
       };
 }
 
-class LinksModel {
-  LinksModel({
+class Links {
+  Links({
     this.products,
     this.subCategories,
   });
 
-  String products;
-  String subCategories;
+  final String products;
+  final String subCategories;
 
-  factory LinksModel.fromJson(Map<String, dynamic> json) => LinksModel(
+  Links copyWith({
+    String products,
+    String subCategories,
+  }) =>
+      Links(
+        products: products ?? this.products,
+        subCategories: subCategories ?? this.subCategories,
+      );
+
+  factory Links.fromJson(String str) => Links.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Links.fromMap(Map<String, dynamic> json) => Links(
         products: json["products"],
         subCategories: json["sub_categories"],
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toMap() => {
         "products": products,
         "sub_categories": subCategories,
       };
